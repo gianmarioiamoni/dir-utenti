f46e96068076528284de6160d6547c7d25aa4ebf
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-11-24 20:56:33 +0100

    add custom grid breakpoint at 320 pixels

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home(): JSX.Element {
       <h1 className="text-2xl font-bold text-center mb-6">Elenco Utenti</h1>
 
       {/* Lista Utenti */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-items-center">
+      <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
         {users?.map((user) => (
           
           // Scheda utente
